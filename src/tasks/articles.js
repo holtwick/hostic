0e77be3dcad19caa62bf51a2c796675fe6c1271e
Dirk Holtwick <dirk.holtwick@gmail.com>
2020-10-22 15:13:09 +0200

    Split up article logic

--- src/tasks/articles.js
+++ src/tasks/articles.js
@@ -1,6 +1,50 @@
 import { parseDate } from '../utils/dateutils.js'
 import { getStat } from '../site/files.js'
 
+export function getArticle({ file, site, routePath = '/' }) {
+  let props = {
+    sourceType: 'html',
+  }
+
+  if (['.md', '.mdown', '.markdown'].includes(file.ext)) {
+    props = site.readMarkdownProperties(file) || {}
+    props.sourceType = 'markdown'
+  }
+
+  let slug = props.slug || file.name
+  slug = slug === 'index' ? '' : slug
+
+  // Hidden
+  if (file.name.startsWith('-')) {
+    props.hidden = true
+    slug = slug.substr(1)
+  }
+
+  // Date
+  let date = parseDate(props.date, file.name) || null
+  if (!date) {
+    const stat = getStat(file.fullPath)
+    date = stat?.mtime
+  }
+  props.date = date || null
+
+  let path = `${routePath}/${slug}`
+  props.path = path
+  props.sourceFile = file
+
+  return { props, path }
+}
+
+export function getArticleBody({ ctx, site }) {
+  if (ctx.sourceType === 'markdown') {
+    ctx.markdown = site.readMarkdown(ctx.sourceFile, { path: ctx.path })
+    ctx.body = ctx.markdown.body
+  } else {
+    ctx.html = site.readHTML(ctx.sourceFile, { path: ctx.path })
+    ctx.body = ctx.html.body
+  }
+}
+
 export function articles({ site, files, handleProps, routePath, body } = {}) {
   let bodyFn = body
 
@@ -8,34 +52,7 @@ export function articles({ site, files, handleProps, routePath, body } = {}) {
 
   files.forEach(file => {
 
-    let props = {
-      sourceType: 'html',
-    }
-
-    if (['.md', '.mdown', '.markdown'].includes(file.ext)) {
-      props = site.readMarkdownProperties(file) || {}
-      props.sourceType = 'markdown'
-    }
-
-    let slug = props.slug || file.name
-    slug = slug === 'index' ? '' : slug
-
-    // Hidden
-    if (file.name.startsWith('-')) {
-      props.hidden = true
-      slug = slug.substr(1)
-    }
-
-    // Date
-    let date = parseDate(props.date, file.name) || null
-    if (!date) {
-      const stat = getStat(file.fullPath)
-      date = stat?.mtime
-    }
-    props.date = date || null
-
-    let path = `${routePath}/${slug}`
-    props.path = path
+    let { props, path } = getArticle({ file, site, routePath })
 
     if (handleProps) {
       if (handleProps(props) === false) {
@@ -47,13 +64,7 @@ export function articles({ site, files, handleProps, routePath, body } = {}) {
       path,
       props,
       ctx => {
-        if (props.sourceType === 'markdown') {
-          ctx.markdown = site.readMarkdown(file, { path: ctx.path })
-          ctx.body = ctx.markdown.body
-        } else {
-          ctx.html = site.readHTML(file, { path: ctx.path })
-          ctx.body = ctx.html.body
-        }
+        getArticleBody({ ctx, site })
         ctx.body = bodyFn(ctx) || ctx.body
       })
   })
